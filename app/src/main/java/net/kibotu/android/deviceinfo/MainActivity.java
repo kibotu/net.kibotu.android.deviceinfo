11d534a642e04e52bf765a9ef61ddac34b07ef88
Jan Rabe <cloudgazer3d@gmail.com>
2016-02-20 20:18:17 +0100

    gradle builds

---
+++ app/src/main/java/net/kibotu/android/deviceinfo/MainActivity.java
@@ -0,0 +1,15 @@
+package net.kibotu.android.deviceinfo;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
